9ddaf7f7b2c5fbcaf2be486bc0009f269cfc557a
Deven Dai <deventhedai@gmail.com>
2026-04-05 17:37:25 -0400

    added win condition and energy combinations

--- datacenter.py
+++ datacenter.py
@@ -3,7 +3,7 @@ from utime import sleep
 import machine, neopixel
 from machine import ADC
 import math
-#import random
+import random
 # test
 
 da = neopixel.NeoPixel(machine.Pin(13), 19)
@@ -11,7 +11,7 @@ en1 = neopixel.NeoPixel(machine.Pin(0), 29)
 en2 = neopixel.NeoPixel(machine.Pin(1), 29)
 
 recieve = Pin(15, Pin.IN)
-
+recieve2 = Pin(12, Pin.IN)
 data_lvl = neopixel.NeoPixel(machine.Pin(14), 10)
 
 def change_green(RGB, length):
@@ -38,52 +38,50 @@ def convert(value):
     volts = value * (3.3 / 65535)
     return volts
 
-oil = 1
-hydro = 2
-wind = 3
-
-solar = 1
-coal = 2
-nuclear = 3
-
-
 while True:
-    """
-    val1 =  random.randint(1,255)
-    val2 =  random.randint(1,255)
-    val3 =  random.randint(1,255)
-    """
-    
     bad = recieve.value()
+    vill_win = recieve2.value()
     #bad = 0
     energy = 0
     
-    data1 = math.floor(convert(ADC(27).read_u16()))
-    data2 = math.floor(convert(ADC(26).read_u16()))
+    data1 = round(convert(ADC(27).read_u16()), 2)
+    data2 = round(convert(ADC(26).read_u16()), 2)
     
-    print(data1)
-    print(data2)
+    #print(data1)
+    #print(data2)
     
-    #check sources  
-    if data1 == oil:
+    #data1 
+    if 1.6 < data1 < 1.7: # wind = 1.6/1.7
+        energy += 2
+    elif data1 > 3.2: #coal = 3.3
         energy += 3
         bad = 1
-    elif data1 == hydro or data1 == wind:
-        energy += 1
+    elif 2.35 < data1 < 2.5: #Hydro = 2.4
+       energy += 2
+    elif 1.75 < data1 < 1.85:
+        energy += 5 #hydro + wind = 1.8
         
-    if data2 == coal:
+    #data2   
+    if 2.3 < data2 < 2.5: #solar = 2.4
+        energy += 1
+    elif data2 > 3.2: #nuclear = 3.3
+        energy += 5
+    elif 1.5 < data2 < 1.8: #oil = 1.6/1.7
         energy += 3
         bad = 1
-    elif data2 == nuclear:
-        energy += 5
-    elif data2 == solar:
-        energy += 1
-    
-    turn_RGB_off(data_lvl, 10)
+    elif 2.15 < data2 < 2.3:
+        energy += 8 # oil + nuclear = 2.7/2.8
+        bad = 1
+    elif 3.0 < data2 < 3.1: # solar + nuclear = 3
+        energy += 6
+        
+    for i in range(10):
+        data_lvl[i] = (0, 0, 0)
     for i in range(min(energy, 10)):
-        data_lvl[i] = (255, 255, 255)
+        data_lvl[i] = (125,125, 125)
     data_lvl.write()
     
+    print(energy)
     #displaying dc lights
     if energy > 10:
         for i in range(19):
@@ -116,14 +114,20 @@ while True:
         change_red(en1, 29)
         change_red(en2, 29)
     else:
-        """
+        change_green(en1, 29)
+        change_green(en2, 29)
+    
+    if energy == 10 and not bad and vill_win:
+        val1 =  random.randint(1,255)
+        val2 =  random.randint(1,255)
+        val3 =  random.randint(1,255)
+        
         for i in range(29):
             en1[i] = (val1, val2, val3)
             en2[i] = (val1, val2, val3)
         en2.write()
         en1.write()
-        """
-        change_green(en1, 29)
-        change_green(en2, 29)
+        
+        sleep(.1)
         
     sleep(.1)
--- village.py
+++ village.py
@@ -6,6 +6,7 @@ import math
 
 vill = neopixel.NeoPixel(machine.Pin(13), 29)
 sender = Pin(15, Pin.OUT)
+win = Pin(12, Pin.OUT)
 # enviroment =  neopixel.NeoPixel(machine.Pin(0), 8)
 # data center = neopixel.NeoPixel(machine.Pin(0), 8)
 # village = neopixel.NeoPixel(machine.Pin(0), 8)
@@ -31,77 +32,81 @@ def turn_RGB_off(RGB, length):
     for i in range(length):
         RGB[i] = (0, 0, 0)
         RGB.write()
+    sleep(.1)
         
 def convert(value):
     volts = value * (3.3 / 65535)
     return volts
 
-#vill1
-coal = 2
-hydro = 1
-wind = 3
-
-#vill2
-solar = 2
-oil = 1
-nuclear = 3
-
 while True:
     sender.value(0)
+    win.value(0)
     energy = 0
     
-    village1 = round(convert(ADC(27).read_u16()))
-    village2 = round(convert(ADC(26).read_u16()))
+    village1 = round(convert(ADC(27).read_u16()), 2)
+    village2 = round(convert(ADC(26).read_u16()), 2)
     
     print(village1)
-    print(village2)
+    #print(village2)
     
-    #village   
-    if village1 == oil:
-        energy += 3
+    #village 1  
+    if 1.5 < village1 < 1.65: #wind = 1.6/1.7
+        energy += 2
+    elif 1.9 < village1 < 2.1: # hydro = 2.0
+        energy += 2
+    elif village1 > 3.2: # oil = 3.3
+        energy += 2
         sender.value(1)
-    elif village1 == hydro or village1 == wind:
-        energy += 1
-
-    if village2 == oil:
-        energy += 3
-        sender.value(1)
-    elif village2 == nuclear:
+    elif 1.7 < village1 < 1.8: # wind + hydro = 1.75
         energy += 5
-    elif village2 == solar:
+        
+    #village 2
+    if 1.9 < village2 < 2.1: #solar = 2.0
         energy += 1
-    
-    print(energy)
-    
-    turn_RGB_off(vill_lvl, 5)
+    elif 2.95 < village2 < 3.1: #nuclear = 3.3
+        energy += 5
+    elif 1.55 < village2 < 1.63: #oil = 1.6
+        energy += 3
+        sender.value(1)
+    elif 1.7 < village2 < 1.8: # oil + solar = 1.8
+        energy += 4
+        sender.value(1)
+    elif 2.1 < village2 < 2.2: # nuclear + oil = 2.1
+        energy += 8
+        sender.value(1)
+    elif 2.8 < village2 < 2.9: # nuclear + solar = 2.8
+        energy = 6
+        
+    for i in range(5):
+        vill_lvl[i] = (0, 0, 0)
+        
     for i in range(min(energy, 5)):
-        vill_lvl[i] = (125, 125, 125)
+        vill_lvl[i] = (150, 0, 150)
     vill_lvl.write()
     
     if energy > 5:
         for i in range(29):
             vill[i] = (125, 0, 0)
         vill.write()
-        sleep(.5)
+        sleep(.3)
         for i in range(29):
             vill[i] = (125, 125, 125)
         vill.write()
-        sleep(.5)
     elif energy == 0:
         turn_RGB_off(vill, 29)
     elif energy == 5:
         change_white(vill, 29)
+        win.value(1)
     elif 0 < energy < 5:
         brightness = int((255 * (energy/10)))
         turn_RGB_off(vill, 29)
         
         for i in range(29):
-            if i % (5 - energy) == 0:
+            if i % (6 - energy) == 0:
                 vill[i] = (brightness, brightness, brightness)
         vill.write()
     else:
         turn_RGB_off(vill, 29)
-    
     
     
     sleep(.5)
